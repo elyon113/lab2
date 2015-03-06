2abe71489631352e523d7d621aa1c17e49e5e7f1
elyon113 <qwerty@123>
2015-03-06 13:32:48 -0800

    Hello world

--- script.sh
+++ script.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-wget -q pogoda.tut.by
-cat index.html | grep -m 1 '<span class="temp-i">.*</span>' | grep -o '[-+][0-9]\+'
+wget -q meteo.by
+cat index.html | grep -m 1 -n6 '<p class="t ">' | grep -o '[-+][0-9]\+'
+
